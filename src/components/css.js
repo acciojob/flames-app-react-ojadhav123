999c6050e8f54cfbd73c1c5ac2f65086ebb8c149
ojadhav123 <omkarjadhav787@gmail.com>
2024-08-28 21:46:48 +0530

    Update css.js

--- src/components/css.js
+++ src/components/css.js
@@ -1,1 +1,17 @@
+.App {
+  text-align: center;
+  margin: 20px;
+}
+
+input {
+  margin: 10px;
+  padding: 5px;
+  font-size: 16px;
+}
+
+button {
+  margin: 10px;
+  padding: 10px;
+  font-size: 16px;
+}
 
